eaa9c4c650abd543a00d8799d860cba6fd7c972b
Massimiliano.arosio <massimiliano.arosio@triboo.it>
2020-03-24 18:06:58 +0100

    added local storege

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@ const listNote = document.querySelector('.content');
 const swithCounterOnOff = false;
 let counter = 0;
 
+//set removeOnClick to 'true' if you want to delete list elements on click on it, else 'false'
+const removeOnClick = true;
+
 //set max number of characters in the input (max number before list start a new paragraph is: maxCharacters = 98)
 const maxCharacters = 64;
 
@@ -30,25 +33,28 @@ inputNewNote.addEventListener("keydown", function (e) {
 function AppNote(content) {
     if (inputNewNote.value.length <= maxCharacters) {
         const newNote = document.createElement('li');
-        newNote.setAttribute("onclick", "removeElement(this)")
+        counter++;
         newNote.textContent = content;
         listNote.appendChild(newNote);
+        localStorage.setItem("list-element-" + counter, content); //set local storage
         inputNewNote.value = "";
+        if (removeOnClick) {
+            newNote.setAttribute("onclick", "removeElement(this)")
+        }
         if (swithCounterOnOff) {
-            counter++;
             newNote.setAttribute("class", "list-" + counter);
         }
-        
+
+        //Il return in genere lo usi quando una funzione deve finire il suo percorso ritornando un valore "preciso" o qualcosa che poi devi usare, quindi poi si ferma. In questo caso puoi anche usarlo ma non hai bisogno anche perché questa è una funzione generica che userai anche dopo 
+
         // append button delete
         let btnRemoveNote = document.createElement('button');
         btnRemoveNote.setAttribute("id", "btnRemove");
         btnRemoveNote.textContent = 'Elimina';
         newNote.appendChild(btnRemoveNote);
-        btnRemoveNote.addEventListener('click', function(){
+        btnRemoveNote.addEventListener('click', function () {
             this.parentNode.remove();
         })
-
-        //Il return in genere lo usi quando una funzione deve finire il suo percorso ritornando un valore "preciso" o qualcosa che poi devi usare, quindi poi si ferma. In questo caso puoi anche usarlo ma non hai bisogno anche perché questa è una funzione generica che userai anche dopo 
 
     } else {
         alert("Text you written is too log, try to write a shorter one next. Text can't be longer that " + maxCharacters + " characters");
